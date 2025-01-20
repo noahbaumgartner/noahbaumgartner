889011c2faccdcae92691583e41fed546857f084
noah <35225015+noahbaumgartner@users.noreply.github.com>
2025-01-20 15:12:50 +0100

    started with hero section

---
+++ src/components/motion/FadeIn.tsx
@@ -0,0 +1,14 @@
+import { motion } from 'motion/react';
+import type { ReactNode } from 'react';
+
+export default function FadeIn({ children }: { children: ReactNode }) {
+    return (
+        <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+        >
+            {children}
+        </motion.div>
+    );
+};
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		colors: {
+			black: '#000',
+			nb: {
+				beige: '#F9F7F1',
+			}
+		},
+		extend: {
+			fontFamily: {
+				poppins: ['Poppins', 'sans-serif'],
+			}
+		},
 	},
 	plugins: [],
 }
